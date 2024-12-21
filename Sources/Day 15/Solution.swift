c7640d882bdfdfd1ad11ec1b21050b15bbd2dd89
peter bohac <gern@1dot0.net>
2024-12-21 08:08:03 -0800

    Stub entry

---
+++ Sources/Day 15/Solution.swift
@@ -0,0 +1,22 @@
+//
+//  Solution.swift
+//  Day 15
+//
+//  Copyright © 2024 peter bohac. All rights reserved.
+//
+
+// MARK: - Part 1
+
+enum Part1 {
+    static func run(_ source: InputData) {
+        print("Part 1 (\(source)):")
+    }
+}
+
+// MARK: - Part 2
+
+enum Part2 {
+    static func run(_ source: InputData) {
+        print("Part 2 (\(source)):")
+    }
+}
