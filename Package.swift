4f1f0ff096046101909cfd98e4719632c46325bc
peter bohac <gern@1dot0.net>
2024-12-20 20:37:57 -0800

    Skeleton package

---
+++ Package.swift
@@ -0,0 +1,111 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AdventOfCode2024",
+    platforms: [.macOS(.v15)],
+    targets: [
+        .executableTarget(
+            name: "Day01",
+            path: "Sources/Day 01"
+        ),
+        .executableTarget(
+            name: "Day02",
+            path: "Sources/Day 02"
+        ),
+//        .executableTarget(
+//            name: "Day03",
+//            path: "Sources/Day 03"
+//        ),
+//        .executableTarget(
+//            name: "Day04",
+//            path: "Sources/Day 04"
+//        ),
+//        .executableTarget(
+//            name: "Day05",
+//            path: "Sources/Day 05"
+//        ),
+//        .executableTarget(
+//            name: "Day06",
+//            path: "Sources/Day 06"
+//        ),
+//        .executableTarget(
+//            name: "Day07",
+//            path: "Sources/Day 07"
+//        ),
+//        .executableTarget(
+//            name: "Day08",
+//            path: "Sources/Day 08"
+//        ),
+//        .executableTarget(
+//            name: "Day09",
+//            path: "Sources/Day 09"
+//        ),
+//        .executableTarget(
+//            name: "Day10",
+//            path: "Sources/Day 10"
+//        ),
+//        .executableTarget(
+//            name: "Day11",
+//            path: "Sources/Day 11"
+//        ),
+//        .executableTarget(
+//            name: "Day12",
+//            path: "Sources/Day 12"
+//        ),
+//        .executableTarget(
+//            name: "Day13",
+//            path: "Sources/Day 13"
+//        ),
+//        .executableTarget(
+//            name: "Day14",
+//            path: "Sources/Day 14"
+//        ),
+//        .executableTarget(
+//            name: "Day15",
+//            path: "Sources/Day 15"
+//        ),
+//        .executableTarget(
+//            name: "Day16",
+//            path: "Sources/Day 16"
+//        ),
+//        .executableTarget(
+//            name: "Day17",
+//            path: "Sources/Day 17"
+//        ),
+//        .executableTarget(
+//            name: "Day18",
+//            path: "Sources/Day 18"
+//        ),
+//        .executableTarget(
+//            name: "Day19",
+//            path: "Sources/Day 19"
+//        ),
+//        .executableTarget(
+//            name: "Day20",
+//            path: "Sources/Day 20"
+//        ),
+//        .executableTarget(
+//            name: "Day21",
+//            path: "Sources/Day 21"
+//        ),
+//        .executableTarget(
+//            name: "Day22",
+//            path: "Sources/Day 22"
+//        ),
+//        .executableTarget(
+//            name: "Day23",
+//            path: "Sources/Day 23"
+//        ),
+//        .executableTarget(
+//            name: "Day24",
+//            path: "Sources/Day 24"
+//        ),
+//        .executableTarget(
+//            name: "Day25",
+//            path: "Sources/Day 25"
+//        ),
+    ]
+)
