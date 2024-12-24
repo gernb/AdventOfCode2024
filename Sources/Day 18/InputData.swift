6a90f1a207620c23049a0cff0c11152490030d01
peter bohac <gern@1dot0.net>
2024-12-24 14:59:38 -0800

    Day 18

--- Sources/Day 18/InputData.swift
+++ Sources/Day 18/InputData.swift
@@ -9,14 +9,16 @@ struct InputData: CustomStringConvertible {
     static let day = 18
     static func part1(_ challengeData: String?) -> [Self?] {[
         .example,
-        challengeData.map { Self(name: "challenge", data: $0) }
+        challengeData.map { Self(name: "challenge", exit: (70,70), bytes: 1024, data: $0) }
     ]}
     static func part2(_ challengeData: String?) -> [Self?] {[
         .example,
-        challengeData.map { Self(name: "challenge", data: $0) }
+        challengeData.map { Self(name: "challenge", exit: (70,70), bytes: 1024, data: $0) }
     ]}
 
     let name: String
+    let exit: (x: Int, y: Int)
+    let bytes: Int
     let data: String
 
     var lines: [String] { data.components(separatedBy: .newlines) }
@@ -24,7 +26,34 @@ struct InputData: CustomStringConvertible {
 
     static let example = Self(
         name: "example",
+        exit: (6,6),
+        bytes: 12,
         data:
 """
+5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0
 """)
 }
--- Sources/Day 18/Solution.swift
+++ Sources/Day 18/Solution.swift
@@ -7,9 +7,81 @@
 
 // MARK: - Part 1
 
+struct Coordinate: Hashable, CustomStringConvertible {
+    var x: Int
+    var y: Int
+
+    static let origin: Self = .init(x: 0, y: 0)
+
+    var description: String { "(\(x), \(y))" }
+
+    var up: Self { .init(x: x, y: y - 1) }
+    var down: Self { .init(x: x, y: y + 1) }
+    var left: Self { .init(x: x - 1, y: y) }
+    var right: Self { .init(x: x + 1, y: y) }
+
+    var adjacent: [Self] { [up, left, right, down] }
+}
+extension Coordinate {
+    init(_ line: String) {
+        let numbers = line.components(separatedBy: ",").compactMap(Int.init)
+        self.init(x: numbers[0], y: numbers[1])
+    }
+}
+
+enum Tile {
+    case empty, corrupted
+}
+
+func findShortestDistance<Node: Hashable>(from start: Node, using getNextNodes: ((Node) -> [Node]?)) -> Int? {
+    var visited: [Node: Int] = [:]
+    var queue: [(node: Node, steps: Int)] = [(start, 0)]
+
+    while queue.isEmpty == false {
+        var (node, steps) = queue.removeFirst()
+        guard let nextNodes = getNextNodes(node) else {
+            return steps
+        }
+        steps += 1
+        for nextNode in nextNodes {
+            if let previousSteps = visited[nextNode], previousSteps <= steps {
+                continue
+            }
+            if queue.contains(where: { $0.node == nextNode } ) {
+                continue
+            }
+            queue.append((nextNode, steps))
+        }
+        visited[node] = steps
+    }
+
+    // No possible path exists
+    return nil
+}
+
 enum Part1 {
+    static func createMemorySpace(_ source: InputData) -> [Coordinate: Tile] {
+        var memory: [Coordinate: Tile] = [:]
+        for x in 0 ... source.exit.x {
+            for y in 0 ... source.exit.y {
+                memory[Coordinate(x: x, y: y)] = .empty
+            }
+        }
+        for index in 0 ..< source.bytes {
+            let coord = Coordinate(source.lines[index])
+            memory[coord] = .corrupted
+        }
+        return memory
+    }
+
     static func run(_ source: InputData) {
-        print("Part 1 (\(source)):")
+        let memory = createMemorySpace(source)
+        let exit = Coordinate(x: source.exit.x, y: source.exit.y)
+        let steps = findShortestDistance(from: Coordinate.origin) { node in
+            if node == exit { return nil }
+            return node.adjacent.filter { memory[$0] == .empty }
+        }!
+        print("Part 1 (\(source)): \(steps)")
     }
 }
 
@@ -17,6 +89,20 @@ enum Part1 {
 
 enum Part2 {
     static func run(_ source: InputData) {
-        print("Part 2 (\(source)):")
+        var memory = Part1.createMemorySpace(source)
+        let exit = Coordinate(x: source.exit.x, y: source.exit.y)
+        for index in source.bytes ..< source.lines.count {
+            let coord = Coordinate(source.lines[index])
+            memory[coord] = .corrupted
+            let steps = findShortestDistance(from: Coordinate.origin) { node in
+                if node == exit { return nil }
+                return node.adjacent.filter { memory[$0] == .empty }
+            }
+            if steps == nil {
+                print("Part 2 (\(source)): \(source.lines[index])")
+                return
+            }
+        }
+        print("Part 2 (\(source)): FAILED")
     }
 }
